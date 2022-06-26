48309a6f55a5ac7f5ff302913ce9212fee3f632d
Hicster Enthusiast <hicster.enthusiast@gmail.com>
2022-06-25 22:17:21 -0500

    updated succesfully to Serenity 0.11

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::{env, collections::HashSet};
+use std::{collections::HashSet, env};
 
 use serenity::{
     async_trait,
+    framework::{standard::macros::group, StandardFramework},
     model::{gateway::Ready, id::UserId},
     prelude::*,
-    framework::{standard::macros::group, StandardFramework},
-    http::Http,
 };
 
 #[group]
@@ -24,31 +23,32 @@ impl EventHandler for Handler {
 }
 #[tokio::main]
 async fn main() {
+    // .env must be in ROOT dir, not in /src
     dotenv::dotenv().expect("Failed to load .env");
     let token = env::var("DTOKEN").expect("Expected token");
-    let owners_ids = env::var("ADMINS")
-        .expect("Expected CSV");
-    
+    let owners_ids = env::var("ADMINS").expect("Expected CSV");
+
     let mut owners = HashSet::new() as HashSet<UserId>;
     for n in owners_ids.split(",") {
-            owners.insert(UserId(n.parse().unwrap()));
+        owners.insert(UserId(n.parse().unwrap()));
     }
 
-    let framework = 
-        StandardFramework::new().configure
-            (|c| c
-                .owners(owners)
-                .prefix(":h"))
-            .group(&GENERAL_GROUP);
+    let intents = GatewayIntents::GUILDS 
+        | GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::GUILD_MESSAGE_TYPING
+        | GatewayIntents::DIRECT_MESSAGES;
 
-    let bot = Http::new_with_token(&token);
-    let mut client = Client::builder(&token)
+    let framework = StandardFramework::new()
+        .configure(|c| c.owners(owners).prefix(":h"))
+        .group(&GENERAL_GROUP);
+
+    let mut client = Client::builder(&token, intents)
         .framework(framework)
         .event_handler(Handler)
         .await
         .expect("Err creating client");
     
-    println!("d");
+
     if let Err(err) = client.start().await {
         panic!("Error starting the client: {:?}", err);
     };
